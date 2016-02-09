f7658520f08877dbf288532b4dd335bac9036dcd
webexninja <shivanktiware@gmail.com>
2016-02-09 05:40:22 +0530

    delete model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209000625) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
