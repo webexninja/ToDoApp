74f82bf6283927d18d4ef252f2b7b0898b98c0fd
webexninja <shivanktiware@gmail.com>
2016-02-09 06:27:27 +0530

    Add index page and write function for display completed and incomplete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,6 @@
 class TasksController < ApplicationController
+	def index
+		@task_c = Task.where(completed: true)
+		@task_i = Task.where(completed: false)
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.delete_all
+
+Task.create!([
+	{id:1, title: "Read Book", completed: false},
+	{id:2, title: "Go to Gym", completed: true},
+	{id:3, title: "Drink 4 Bottles of Water", completed: false},
+	{id:4, title: "Finish Work by 6", completed: false},
+	{id:5, title: "Write Code", completed: false},
+])
